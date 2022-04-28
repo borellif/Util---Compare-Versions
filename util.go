640bea7ba152f240dded76fdff9c87501a45139a
borellif <fborelli.alt@gmail.com>
2022-04-27 21:27:12 -0400

    Comments and converts string values to integers

--- util.go
+++ util.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -17,6 +18,8 @@ func main() {
 }
 
 func compareVersions(version1, version2 string) (int, error) {
+	// If for some reason version 1 or version 2 are null, throw an error
+	//	and provide context as to which version is null or blank
 	if version1 == "" {
 		var b strings.Builder
 		b.WriteString("Version 1 is null or blank")
@@ -27,14 +30,23 @@ func compareVersions(version1, version2 string) (int, error) {
 		return 0, errors.New(b.String())
 	}
 
+	// Split each of the strings by the dot character into a String array
 	splitVar1 := strings.Split(version1, ".")
 	splitVar2 := strings.Split(version2, ".")
 
-	for index, value := range splitVar1 {
-		if value > splitVar2[index] {
+	// Start to iterate through the first split version string in order to compare the versions
+	for index, strValue := range splitVar1 {
+
+		// values are still in String form so we want to convert them
+		currentVersion1, err1 := strconv.Atoi(strValue)
+		currentVersion2, err2 := strconv.Atoi(splitVar2[index])
+
+		if (err1 == nil) && (err2 == nil) {
+			if currentVersion1 > currentVersion2 {
 				return 1, nil
-		} else if value < splitVar2[index] {
+			} else if currentVersion1 < currentVersion2 {
 				return -1, nil
+			}
 		}
 	}
 
